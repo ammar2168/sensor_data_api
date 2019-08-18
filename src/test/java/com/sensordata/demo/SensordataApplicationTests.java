be2a8f5aa8f2bb78d7c406e6be9a6756802cf355
Ammar <ammar@shoonya.io>
2019-08-18 21:34:52 +0530

    add docker compose file for running kafka

--- src/test/java/com/sensordata/demo/SensordataApplicationTests.java
+++ src/test/java/com/sensordata/demo/SensordataApplicationTests.java
@@ -9,16 +9,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.Statement;
 import java.util.Iterator;
 
 @RunWith(SpringRunner.class)
